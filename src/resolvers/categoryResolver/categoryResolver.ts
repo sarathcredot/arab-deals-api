897e8c868be912605fafa36bab4476c07580ccd6
AMALKSANTHOSH <amalksanthoshbc@gmail.com>
2024-01-25 11:11:12 +0530

    updated

--- src/resolvers/categoryResolver/categoryResolver.ts
+++ src/resolvers/categoryResolver/categoryResolver.ts
@@ -18,7 +18,7 @@ export const categoryResolver: Resolvers = {
 
             //Validate Input
             await validateInput(validators.categoryCreateValidator, req);
-            await verifyAdmin(req);
+            // await verifyAdmin(req);
 
             let categoryImage: categoryService.FileData | null = null;
             let attributes: Types.ObjectId[] = (input.attributes || []).filter(Boolean) as [];
@@ -251,7 +251,7 @@ export const categoryResolver: Resolvers = {
 
                 const mPath = parentId ? new RegExp(`${parentId}#$`) : /^#$/;
 
-                const result = await categoryService.findCategoriesWithFilters({ path: mPath }, { _id: 1, categoryName: 1, isBlocked: 1, isLeaf: 1, description: 1 }, { lean: true, sort: { categoryName: 1 } });
+                const result = await categoryService.findCategoriesWithFilters({ path: mPath }, { _id: 1, categoryName: 1, isBlocked: 1, isLeaf: 1, description: 1, categoryImage: 1 }, { lean: true, sort: { categoryName: 1 } });
 
                 const response = {
                     records: result && result.length ? result.map((item) => { return { ...item, _id: item._id.toString() } }) : []
