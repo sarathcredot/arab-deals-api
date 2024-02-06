41fa312a477383e5cd03477c6acbe7c3f4654e80
Amalksanthoshcredot <amalksanthosh@credot.dev>
2024-02-06 11:16:25 +0530

    updated

---
+++ src/models/cmsModel.ts
@@ -0,0 +1,88 @@
+import { Schema, model } from "mongoose";
+import { collections } from "../configs";
+
+
+const fileSchema = new Schema(
+    {
+        fileType: {
+            type: String,
+            enum: ["PRIVATE", "PUBLIC"],
+            default: "PUBLIC",
+            required: true
+        },
+        fileURL: {
+            type: String,
+            required: true
+        },
+        mimeType: {
+            type: String,
+            required: true
+        },
+        originalName: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        _id: true,
+        timestamps: true
+    }
+);
+
+const buttonSchema = new Schema(
+    {
+        buttonText: {
+            type: String,
+            required: true
+        },
+        redirectionURL: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        _id: true,
+        timestamps: true
+    }
+);
+
+const cmsSchema = new Schema(
+    {
+        pageName: {
+            type: String
+        },
+        sectionName: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        title: {
+            type: String
+        },
+        subTitle: {
+            type: String
+        },
+        description: {
+            type: [String]
+        },
+        images: {
+            type: [fileSchema],
+            deafult: []
+        },
+        buttons: {
+            type: [buttonSchema],
+            default: []
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+const cmsModel = model(collections.CMS, cmsSchema);
+
+
+export {
+    cmsModel
+}
