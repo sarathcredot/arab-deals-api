f3e1492f53f991aa7592bd5e915aaeb85ecaa7b9
ukeh <ukeh.hari@gmail.com>
2024-03-09 09:21:11 +0530

    updated

--- src/scripts/copyFiles.ts
+++ src/scripts/copyFiles.ts
@@ -2,10 +2,15 @@ const copyfiles = require('copyfiles');
 
 // Copy files from 'uploads/' to 'dist/'
 copyfiles(['uploads/*', 'dist/uploads'], { up: 1 }, () => {
-  console.log('Files copied from "uploads/" to "dist/".');
+  console.log('Files copied from "uploads/" to "dist/uploads".');
 });
 
 // Copy files from 'public/' to 'dist/'
 copyfiles(['public/*', 'dist/public'], { up: 1 }, () => {
-  console.log('Files copied from "public/" to "dist/".');
+  console.log('Files copied from "public/" to "dist/public".');
+});
+
+// Copy files from 'src/files' to 'dist/'
+copyfiles(['exports/*', 'dist/exports'], { up: 1 }, () => {
+  console.log('Files copied from "exports/" to "dist/exports".');
 });
