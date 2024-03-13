cd63992a283485dc69511d8c6b1ee563c11ff003
ukeh <ukeh.hari@gmail.com>
2024-03-13 10:42:33 +0530

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Express } from 'express';
 import { testRouter } from './testRouter';
 import { welcomeRouter } from './welcomeRouter';
+import { fileRouter } from './fileRouter';
 
 
 export default (app: Express) => {
     app.use("/", welcomeRouter);
     app.use("/test", testRouter);
+    app.use("/file", fileRouter);
 }
