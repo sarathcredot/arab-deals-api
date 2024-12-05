22750c1c79279862981a256620433883ab81deaa
sarathcredot <sarath@credot.in>
2024-12-05 11:36:39 +0530

    error code

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -22,7 +22,6 @@ import { dashboardResolver } from "./dashboardResolver/dashboardResolver";
 import { jobResolver } from "./jobResolver/jobResolver";
 import { scalarTypeResolver } from "./scalarTypeResolver/scalarTypeReolver";
 import { settingsResolver } from "./settingsResolver/settingsResolver";
-import {deliveryAgentResolver} from './deliveryAgentResolver/deliveryAgentResolver'
 
 
 
@@ -49,6 +48,5 @@ export const resolvers: Resolvers = mergeResolvers(
         dashboardResolver,
         jobResolver,
         settingsResolver,
-        deliveryAgentResolver
     ]
 );
